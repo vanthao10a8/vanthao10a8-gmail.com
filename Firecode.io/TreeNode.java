46d14b895b9857eb9021f778cc439349c400c621
Thao Nguyen <vanthao10a8@gmail.com>
2020-04-10 00:45:13 +0700

    NumberOfLeaves

---
+++ Firecode.io/TreeNode.java
@@ -0,0 +1,10 @@
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int data, TreeNode left, TreeNode right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+    }
+}  
